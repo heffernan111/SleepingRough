399ddb9de0fc0aacaa1c8075b036df31764d05f2
Unknown <david.heff.dh@googlemail.com>
2017-08-06 12:25:42 +0100

    Initial.

---
+++ app/src/main/java/com/artesseum/sleepingrough/Tab2NewStart.java
@@ -0,0 +1,23 @@
+package com.artesseum.sleepingrough;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by apoca on 06/08/2017.
+ */
+
+public class Tab2NewStart extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2newstart, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/artesseum/sleepingrough/Tab3Volunteer.java
@@ -0,0 +1,23 @@
+package com.artesseum.sleepingrough;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by apoca on 06/08/2017.
+ */
+
+public class Tab3Volunteer extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3volunteer, container, false);
+        return rootView;
+    }
+}
